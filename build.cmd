976fd14de7208bae7299f15ef523e21215054c53
franklin02 <luyang0309@gmail.com>
2021-04-27 23:04:07 -0600

    Signed-off-by: franklin02 <luyang0309@gmail.com>

--- build.cmd
+++
@@ -1,4 +0,0 @@
-cd Livox-SDK
-cd build
-cmake .. -G "Visual Studio 16 2019" -A x64 -or (echo "Livox Build Failed";  exit 1)
-exit 0
--- src/MatLab/ptCloudToPCD.m
+++
@@ -1,9 +0,0 @@
-function ptCloudToPCD(ptCld, path, name)
-%ptcellArrayToPCD Summary of this function goes here
-%   Converts a cell array of point clouds to a PCD sequence in a new folder
-%   ptcell : 
-mkdir(path);
-pcwrite(ptCld, path + '\' + name + '.pcd', 'Encoding', 'compressed')
-
-
-end
